9b9e6f21d05141570eb5f10e434d990f55455983
Suleman Rufai <srufai100@gmail.com>
2023-10-29 15:04:33 -0700

    A programme to print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- program entry point
+ * @argc: main parameter known as argument count;
+ * @argv: also a parameter known as argument vector;
+ * Return: (0) at success;
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+	printf("%s\n", argv[0]);
+	}
+return (0);
+}
