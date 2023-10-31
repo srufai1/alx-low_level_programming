f17a7d3c15d5beffe795f6172014fc22cfee5e8e
Suleman Rufai <srufai100@gmail.com>
2023-10-31 03:26:09 -0700

    a fucntion to return a pointer toa newly allocared space in memory fixing requiremwnt

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,23 +6,23 @@
 */
 char *_strdup(char *str)
 {
-  char *ptr_string;
-  int count = 0, i = 0;
+char *ptr_string;
+int count = 0, i = 0;
 
-  if (str == NULL)
-    return (NULL);
-  while (str[count] != '\0')
-    count++;
+if (str == NULL)
+return (NULL);
+while (str[count] != '\0')
+count++;
 
-  ptr_string = malloc(sizeof(char) * (count + 1));
+ptr_string = malloc(sizeof(char) * (count + 1));
 
-  if (ptr_string == NULL)
-    return (NULL);
+if (ptr_string == NULL)
+return (NULL);
 
-  for (; str[i]; i++)
-    ptr_string[i] = str[i];
+for (; str[i]; i++)
+ptr_string[i] = str[i];
 
-  return (ptr_string);
+return (ptr_string);
 
-  free(ptr_string);
+free(ptr_string);
 }
