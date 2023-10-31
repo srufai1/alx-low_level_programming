9ee98bc084cb2afd1e40a77c596b918d12894ea1
Suleman Rufai <srufai100@gmail.com>
2023-10-31 03:10:48 -0700

    fixing a requirement

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	array = (char *)malloc(size * sizeof(char));
 
 
-	if(size  == 0 || array == NULL)
+	if (size  == 0 || array == NULL)
 		return (NULL);
 	for (; i < size; i++)
 	{
