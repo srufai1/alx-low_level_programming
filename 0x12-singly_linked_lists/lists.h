afb6a3f2dbfc2641dd76203f0297a2a4d3558f71
Suleman Rufai <srufai100@gmail.com>
2023-11-16 14:56:19 -0800

    header file corrections

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,16 +7,16 @@
 
 /**
  * struct list_s -singly linked list
- * @n: integer
+ * @len: integer
  * @str: flexible string
  * @next: points to the next node
  * Description: singly linked list node structure for this project
  */
 typedef struct list_s
 {
- char *str;
- unsigned int len;
- struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
