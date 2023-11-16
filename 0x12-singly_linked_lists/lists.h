9db480a00da5c8624e9a6e7717973e271f817086
Suleman Rufai <srufai100@gmail.com>
2023-11-16 14:02:41 -0800

    requiremts corrections

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s -singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure for this project
+ */
+typedef struct list_s
+{
+        char *str;
+        int len;
+        struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
